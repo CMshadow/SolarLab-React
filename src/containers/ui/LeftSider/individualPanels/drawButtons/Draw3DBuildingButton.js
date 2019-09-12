becd7751e88eeceec7441b89599ee9bd5487daa3
XIN JIN <jinxin2457@gmail.com>
2019-09-12 00:02:15 -0400

    平屋顶建模（有bug）

---
+++ src/containers/ui/LeftSider/individualPanels/drawButtons/Draw3DBuildingButton.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/pro-light-svg-icons'
+import {
+  Divider,
+  Row,
+  Col,
+  Button,
+} from 'antd';
+
+import * as actions from '../../../../../store/actions/index';
+
+
+const draw3DBuildingButton = (props) => { 
+    let buildingFoundationHeight = null;
+    let buildingCoordinatesArray = null;
+    let buildingFoundation = null;
+    let drawingBuildingFoundation = null;
+
+
+    const DrawBuildingPolygon = (
+        <Button
+            type = 'primary'
+            size = 'large'
+            shape = 'round'
+            block
+            onClick = {() => {
+                console.log('[Button]: Test Polygon: ');
+                props.EnablePolygon();
+                buildingFoundationHeight = props.BuildingInfoFields.foundHt;
+                buildingCoordinatesArray= props.BuildFoundation.getPointsCoordinatesArray();
+                let buildingCoordinatesSize = buildingCoordinatesArray.length;
+                buildingCoordinatesArray.splice(buildingCoordinatesSize - 2,3);
+                
+                buildingFoundation = props.CreateBuildingFoundationPolygon(buildingFoundationHeight, buildingCoordinatesArray);
+                console.log('Starting Building' + props.DrawingBuidingFoundation.height);
+
+            }}
+        >Test: Draw Foundation</Button>
+    );
+    
+    return (
+        <Row>
+        <Col span={18} offset={3}>
+          {DrawBuildingPolygon}
+        </Col>
+      </Row>
+    );
+    
+}
+
+const mapStateToProps = state => {
+    return {
+        BuildingInfoFields: state.buildingManagerReducer.buildingInfoFields,
+        BuildFoundation: state.undoableReducer.present.drawingManagerReducer.drawingPolyline,
+        CreateBuildingFoundationPolygon: state.undoableReducer.present.drawingPolygonManagerReducer.createBuildingFoundationPolygon,
+        DrawingBuidingFoundation: state.undoableReducer.present.drawingPolygonManagerReducer.BuildingFoundation
+
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+        EnablePolygon: () => dispatch(actions.enableToBuildFoundation()),
+        CreateBuildingFoundationPolygon: (newHeight, coordinatesArray) => 
+                dispatch(actions.createPolygonFoundation(newHeight, coordinatesArray))
+    };
+  };
+
+export default connect(mapStateToProps,mapDispatchToProps)(draw3DBuildingButton);
