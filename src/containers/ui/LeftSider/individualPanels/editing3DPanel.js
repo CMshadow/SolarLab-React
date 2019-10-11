3ca4229f4edd2967e8ca614c05479f626f861ddd
CMshadow <bjwmc1996@gmail.com>
2019-10-11 00:04:42 -0700

    TESTING martinez polygon clipping

--- src/containers/ui/LeftSider/individualPanels/editing3DPanel.js
+++ src/containers/ui/LeftSider/individualPanels/editing3DPanel.js
@@ -4,6 +4,8 @@ import {
   Divider,
   Button
 } from 'antd';
+import * as martinez from 'martinez-polygon-clipping';
+
 import RoofList3D from './edit3D/roofList3D';
 import KeepoutList3D from './edit3D/keepoutList3D';
 import FinishedModelingButton from './drawButtons/finishModelingButton';
@@ -13,12 +15,18 @@ const DrawBuildingPanel = (props) => {
   const generateDiffGeometry = (props) => {
     const geoFoundation =
       props.workingBuilding.foundationPolygonExcludeStb.map(polygon =>
-        polygon.makeGeoJSON()
+        polygon.toFoundLine().makeGeoJSON()
       );
     const geoNormalKeepout =
-      props.allNormalKeepout.map(kpt => kpt.outlinePolyline.makeGeoJSON())
+      props.allNormalKeepout.map(kpt => kpt.keepout.outlinePolygonPart2.toFoundLine().makeGeoJSON())
+    const geoPassageKeepout =
+      props.allPassageKeepout.map(kpt => kpt.keepout.outlinePolygon.toFoundLine().makeGeoJSON())
     console.log(geoFoundation)
     console.log(geoNormalKeepout)
+    console.log(geoPassageKeepout)
+    let a = martinez.union(geoPassageKeepout[0].geometry.coordinates,geoNormalKeepout[0].geometry.coordinates)
+    a= martinez.diff(geoFoundation[0].geometry.coordinates,a)
+    console.log(a)
   }
 
   const testButton = (
@@ -49,7 +57,8 @@ const mapStateToProps = state => {
   return {
     uiState: state.undoableReducer.present.uiStateManagerReducer.uiState,
     workingBuilding: state.buildingManagerReducer.workingBuilding,
-    allNormalKeepout: state.keepoutManagerReducer.normalKeepout
+    allNormalKeepout: state.keepoutManagerReducer.normalKeepout,
+    allPassageKeepout: state.keepoutManagerReducer.passageKeepout
   };
 };
 
