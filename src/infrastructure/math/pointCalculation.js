b5820b9cdd6ba4495f21f2b0d07b01d64db6f6f9
Hard-To-Name <leuchteintraum@gmail.com>
2019-12-10 18:09:11 -0800

    shadow length attempt

--- src/infrastructure/math/pointCalculation.js
+++ src/infrastructure/math/pointCalculation.js
@@ -2,7 +2,8 @@ import * as Cesium from 'cesium';
 
 import Coordinate from '../point/coordinate';
 import Point from '../point/point';
-import { cartesianToPoint, getPlaneEquationForPoint, getPlaneEquationForCartesian, getPlaneLineIntersectPointPosition, rotatePointWrapper } from './shadowHelper';
+import { calculateSunPositionWrapper } from './sunPositionCalculation';
+import { cartesianToPoint, getPlaneEquationForPoint, getPlaneEquationForCartesian, getPlaneLineIntersectPointPosition, rotatePointWrapper, getRatio, shadow_vector } from './shadowHelper';
 
 /**
  * [minPanelTiltAngleOnPitchedRoof description]
@@ -66,4 +67,64 @@ export const minPanelTiltAngleOnPitchedRoof = (plane_points, panelBrng) => {
     const minPanelTilt = Math.acos(cos_theta);
 
     return minPanelTilt * 180 / Math.PI;
+}
+
+const getHighAndFixedIndex = (panel_points, p_ratio) => {
+    for (var i = 1; i < 4; ++i) {
+        var delta_lon = panel_points[0].lon - panel_points[i].lon;
+        var delta_lat = panel_points[0].lat - panel_points[i].lat;
+        var delta_height = panel_points[0].height - panel_points[i].height;
+
+        if (delta_lon / p_ratio[0] === delta_height && delta_lat / p_ratio[1] === delta_height) {
+            if (delta_height < 0) return [0, i];
+            else return [i, 0];
+        }
+    }
+    return null;
+}
+
+/**
+ * [minPanelTiltAngleOnPitchedRoof description]
+ * @param  {Point[]} plane_points  斜屋面顶点集
+ * @param  {Point[]} panel_points  太阳能板顶点集 in degrees
+ * @param  {Number} panel_al       太阳能板高度角 in degrees
+ * @param  {Number} panel_az       太阳能板方位角
+ * @return {Number}                阴影长度 in meters
+ */
+export const calculatePanelShadowLength = (plane_points, panel_points, panel_al, panel_az) => {
+
+    const ratio = getRatio(plane_points[0].lon, plane_points[0].lat);
+
+    const panel_position = [panel_al, panel_az];
+    const p_vec = shadow_vector(panel_position);
+    const p_ratio = [ratio[0] * p_vec[0], ratio[1] * p_vec[1]];
+
+    const panel_pair = getHighAndFixedIndex(panel_points, p_ratio);
+    if (panel_pair === null) {
+        console.log("error");
+        return null;
+    }
+    const high_point = panel_points[panel_pair[0]];
+    const fix_point = panel_points[panel_pair[1]];
+
+    const solar_position = calculateSunPositionWrapper();
+    const s_vec = shadow_vector(solar_position);
+    const s_ratio = [ratio[0] * s_vec[0], ratio[1] * s_vec[1]];
+
+    const plane_equation = getPlaneEquationForPoint(plane_points[0], plane_points[1], plane_points[2]);
+
+    const ref_point = new Point(high_point.lon + s_ratio[0], high_point.lat + s_ratio[1], high_point.height - 1);
+    const joint = getPlaneLineIntersectPointPosition(high_point, ref_point, plane_equation);
+    const joint_cartesian = Cesium.Cartesian3.fromDegrees(
+        joint.lon, joint.lat, joint.height
+    );
+    const fix_cartesian = Cesium.Cartesian3.fromDegrees(
+        fix_point.lon, fix_point.lat, fix_point.height
+    );
+
+    const delta_x = fix_cartesian.x - joint_cartesian.x;
+    const delta_y = fix_cartesian.y - joint_cartesian.y;
+    const delta_z = fix_cartesian.z - joint_cartesian.z;
+
+    return Math.sqrt(delta_x * delta_x + delta_y * delta_y + delta_z * delta_z);
 }
