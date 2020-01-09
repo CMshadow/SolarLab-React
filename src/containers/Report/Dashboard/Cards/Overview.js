da87a2359574f5a18a8f4d41a146eb703edc90b6
CMshadow <bjwmc1996@gmail.com>
2020-01-10 03:26:05 +0800

    in progress

--- src/containers/Report/Dashboard/Cards/Overview.js
+++ src/containers/Report/Dashboard/Cards/Overview.js
@@ -6,57 +6,57 @@ import Numeral from "numeral";
 const { Title } = Typography;
 
 class Overview extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { isImageLoaded: false };
-        this.handleImageLoadedEvent = this.handleImageLoadedEvent.bind(this);
-        this.imageRef = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.state = { isImageLoaded: false };
+    this.handleImageLoadedEvent = this.handleImageLoadedEvent.bind(this);
+    this.imageRef = React.createRef();
+  }
 
-    handleImageLoadedEvent() {
-        this.setState({ isImageLoaded: true });
-    }
+  handleImageLoadedEvent() {
+    this.setState({ isImageLoaded: true });
+  }
 
-    render () {
-        return (
-            <Col span={24}>
-                <Card
-                    style={{ height: '100%', cursor: 'default' }}
-                    hoverable={ true }
-                    loading={ !this.props.metadata.loaded }
-                >
-                    <Title>{ this.props.metadata.loaded ? this.props.metadata.option.data['Project_Name'] : 'Loading...' }</Title>
-                    <Divider />
+  render () {
+    return (
+      <Col span={24}>
+        <Card
+          style={{ height: '100%', cursor: 'default' }}
+          hoverable={ true }
+          loading={ !this.props.metadata.loaded }
+        >
+          <Title>{ this.props.metadata.loaded ? this.props.metadata.option.data['Project_Name'] : 'Loading...' }</Title>
+          <Divider />
 
-                    <Row type="flex" gutter={[16, 16]}>
-                        <Col span={18}>
-                            { this.props.metadata.loaded &&
-                            <Descriptions title="Overview" size='small' layout="vertical" bordered>
-                                <Descriptions.Item label={<b>Project Name</b>} >{ this.props.metadata.option.data['Project_Name'] }</Descriptions.Item>
-                                <Descriptions.Item label={<b>Address</b>} >{ this.props.metadata.option.data['Address'] }</Descriptions.Item>
-                                <Descriptions.Item label={<b>System Efficiency</b>} >{ Numeral(this.props.metadata.option.data['System_Efficiency']).format('0,0.0') }%</Descriptions.Item>
-                                <Descriptions.Item label={<b>kWh/kWp</b>} >{ Numeral(this.props.metadata.option.data['kWh/kWp']).format('0,0.0') }</Descriptions.Item>
-                                <Descriptions.Item label={<b>LCOE</b>} >{ Numeral(this.props.metadata.option.data['LCOE']).format('0,0.0') }</Descriptions.Item>
-                                <Descriptions.Item label={<b>Payback Period</b>} >{ Numeral(this.props.metadata.option.data['Payback_period']).format('0,0.0') } years</Descriptions.Item>
-                                <Descriptions.Item label={<b>Updated by</b>} >{ UtilFunctions.isoStringParser(this.props.metadata.option.data['Project_Updated_Time']) }</Descriptions.Item>
-                            </Descriptions> }
-                        </Col>
-
-                        <Col span={6}>
-                            <Descriptions title="Layout" />
-                            <Skeleton loading={ !this.state.isImageLoaded } active />
-                        </Col>
-                    </Row>
-                </Card>
+          <Row type="flex" gutter={[16, 16]}>
+            <Col span={18}>
+              { this.props.metadata.loaded &&
+              <Descriptions title="Overview" size='small' layout="vertical" bordered>
+                <Descriptions.Item label={<b>Project Name</b>} >{ this.props.metadata.option.data['Project_Name'] }</Descriptions.Item>
+                <Descriptions.Item label={<b>Address</b>} >{ this.props.metadata.option.data['Address'] }</Descriptions.Item>
+                <Descriptions.Item label={<b>System Efficiency</b>} >{ Numeral(this.props.metadata.option.data['System_Efficiency']).format('0,0.0') }%</Descriptions.Item>
+                <Descriptions.Item label={<b>kWh/kWp</b>} >{ Numeral(this.props.metadata.option.data['kWh/kWp']).format('0,0.0') }</Descriptions.Item>
+                <Descriptions.Item label={<b>LCOE</b>} >{ Numeral(this.props.metadata.option.data['LCOE']).format('0,0.0') }</Descriptions.Item>
+                <Descriptions.Item label={<b>Payback Period</b>} >{ Numeral(this.props.metadata.option.data['Payback_period']).format('0,0.0') } years</Descriptions.Item>
+                <Descriptions.Item label={<b>Updated by</b>} >{ UtilFunctions.isoStringParser(this.props.metadata.option.data['Project_Updated_Time']) }</Descriptions.Item>
+              </Descriptions> }
             </Col>
-        )
-    }
+
+            <Col span={6}>
+              <Descriptions title="Layout" />
+              <Skeleton loading={ !this.state.isImageLoaded } active />
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+    )
+  }
 }
 
 
 // pass data to props
 function mapStateToProps(state) {
-    return { metadata: state.undoableReducer.present.reportManager.metadata }
+  return { metadata: state.undoableReducer.present.reportManager.metadata }
 }
 
 export default connect(mapStateToProps)(Overview);
--- src/containers/Report/Dashboard/Dashboard.js
+++ src/containers/Report/Dashboard/Dashboard.js
@@ -11,109 +11,75 @@ import PVProduction from "./Cards/PVProduction";
 import Losses from "./Cards/Losses";
 import Equipment from "./Cards/Equipment";
 import DateStatisticSelectGroup from "./Cards/DateStatisticSelectGroup";
-import Preview from "./PrintPreview";
 
 import './print-display.css';
 
 const { Header, Footer, Content } = Layout;
 
 class Dashboard extends Component {
-  showPrintWindow = () => {
-    if (document.readyState === "complete") {
-      // show print window
-      window.print();
-    }
-    else {
-      // show notification
-      notification.warning({
-        message: 'Still Loading...',
-        description: 'The page is still loading. Please wait until data has been fully loaded. ',
-        placement: 'bottomRight',
-      });
-    }
-  };
 
   render () {
     return (
-        <>
-          <Layout className="non-print" style={{paddingTop: 64, overflow: 'auto'}}>
-            <Header style={{height: 64, backgroundColor: '#fff', boxShadow: '0 2px 8px #f0f1f2' }}>
-              <a href="/" style={{ float: 'left', display: 'block', height: 32, lineHeight: '32px', margin: 16 }}>
-                <img src={logo} alt="logo" style={{ height: 32 }}/>
-              </a>
+      <Layout id='report-layout' className="non-print" style={{paddingTop: 64, overflow: 'auto'}}>
+        <Header id='report-header' data-html2canvas-ignore="true" style={{height: 64, backgroundColor: '#fff', boxShadow: '0 2px 8px #f0f1f2' }}>
+          <a href="/" style={{ float: 'left', display: 'block', height: 32, lineHeight: '32px', margin: 16 }}>
+            <img src={logo} alt="logo" style={{ height: 32 }}/>
+          </a>
 
-              <Menu
-                theme="light"
-                mode="horizontal"
-                selectable={false}
-                style={{ lineHeight: '63px', borderBottom: 'none', float: 'right' }}
-              >
-                <Menu.Item key="1" onClick={() => {
-                  const element = document.getElementById('myoverview');
-                  const worker = new html2pdf.Worker();
-                  const opt = {
-                    pagebreak: { mode: ['avoid-all'], avoid: ['#myoverview'] },
-                    jsPDF: { orientation: 'l' }
-                  };
-                  worker.set(opt).from(element).save();
-                  // this.showPrintWindow()
-                }}>
-                  <Icon type="download"/> Download PDF
-                </Menu.Item>
-                <Menu.Item key="2" onClick={() => {
-                  const element = document.getElementById('report');
-                  console.log(element.width)
-                  console.log(element)
-                  // const worker = new html2pdf.Worker();
-                  // worker.from(element).toImg().outputImg().then(img => window.print(img));
-                  html2canvas(element).then(function (canvas) {
-                  	document.body.appendChild(canvas);
-                  });
-                }}>
-                  TEST
-                </Menu.Item>
-              </Menu>
-            </Header>
+          <Menu
+            theme="light"
+            mode="horizontal"
+            selectable={false}
+            style={{ lineHeight: '63px', borderBottom: 'none', float: 'right' }}
+          >
+            <Menu.Item key="1" onClick={() => {
+              const element = document.getElementById('report-layout');
+              const worker = new html2pdf.Worker();
+              const opt = {
+                pagebreak: { mode: 'avoid-all', avoid: ['#report-layout', '#report-header', '#report','#myoverview', '#test'] },
+                // html2canvas: {ignoreElements: elem => {return elem.id !== 'report-header'}},
+                jsPDF: { orientation: 'l' }
+              };
+              worker.set(opt).from(element).save();
+            }}>
+              <Icon type="download"/> Download PDF
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <br/>
 
-            <Row type='flex'>
-              <Content id='report' style={{ maxWidth: 1000, margin: 'auto', align:'center', textAlign:'center'}}>
-                <Row id='myoverview' type="flex" justify='center' gutter={[16, 16]}>
-                  <Overview />
-                </Row>
+        <Content id='report' style={{ maxWidth: 1000, margin: 'auto', align:'center', textAlign:'center'}}>
+          <Row id='myoverview' type="flex" justify='center' gutter={[16, 16]}>
+            <Overview />
+          </Row>
 
-                <Row type="flex" justify='center' gutter={[16, 16]}>
-                  <IconSummary span={1} type='panel'  />
-                  <IconSummary span={1} type='dollar' />
-                  <IconSummary span={1} type='factory'/>
-                  <IconSummary span={1} type='house'  />
-                </Row>
+          <Row id='test' type="flex" justify='center' gutter={[16, 16]}>
+            <IconSummary span={1} type='panel'  />
+            <IconSummary span={1} type='dollar' />
+            <IconSummary span={1} type='factory'/>
+            <IconSummary span={1} type='house'  />
+          </Row>
 
-                <Row type="flex" justify='center' gutter={[16, 16]} >
-                  <Financial />
-                </Row>
-                <Row type="flex" justify='center' gutter={[16, 16]} >
-                  <PVProduction />
-                  <Losses />
-                </Row>
+          <Row type="flex" justify='center' gutter={[16, 16]} >
+            <Financial />
+          </Row>
+          <Row type="flex" justify='center' gutter={[16, 16]} >
+            <PVProduction />
+            <Losses />
+          </Row>
 
-                <Row type="flex" justify='center' gutter={[16, 16]}>
-                  <DateStatisticSelectGroup span={4} />
-                  <Equipment span={4} />
-                </Row>
-              </Content>
-            </Row>
+          <Row type="flex" justify='center' gutter={[16, 16]}>
+            <DateStatisticSelectGroup span={4} />
+            <Equipment span={4} />
+          </Row>
+        </Content>
 
-            <Footer style={{ textAlign: 'center' }}>
-              SolarLab ©2019 Powered by Albedo Inc.
-            </Footer>
+        <Footer style={{ textAlign: 'center' }}>
+          SolarLab ©2019 Powered by Albedo Inc.
+        </Footer>
 
-            <BackTop />
-          </Layout>
-
-          <div className="only-print">
-            <Preview />
-          </div>
-        </>
+        <BackTop />
+      </Layout>
     )
   }
 }
